092fc34fd475843d15c5c94322ba5b8977570ba9
Marco Visser <marco.d.visser@gmail.com>
2013-09-03 15:47:48 +0200

    aprof table explanations changes with Eelke's suggestions

--- R/aprof.R
+++ R/aprof.R
@@ -394,23 +394,23 @@ aprof<-function(calls,interval,type="line"){
 
 	# limits of Amdahl's law as S goes to inf
 	SpeedTable<-cbind(SpeedTable,1/(1-PropLines))
-	dimnames(SpeedTable)<-list(paste("Max Speed-up line*:", 
+	dimnames(SpeedTable)<-list(paste("Line*:", 
 	LineProf$Line.Numbers,":"),c(Speedups,"S -> Inf**"))
 	SpeedTable<-SpeedTable[order(PropLines,decreasing=TRUE),]
 
-	dimnames(ExecTimeTable)<-list(c(paste("Improvemnt in time line*:", 
+	dimnames(ExecTimeTable)<-list(c(paste("Line*:", 
 	LineProf$Line.Numbers,":"),"All lines"),Speedups)
 	ExecTimeTable<-ExecTimeTable[order(
 	c(PropLines,sum(PropLines)),decreasing=TRUE),]
 
+        cat(" Largest attainable speed-up factor for the entire program \n
+        when 1 line is sped-up with factor (S): \n\n"
 
-        cat("\n Maximum theoretical attainable speed-up per line number:\n\n")
-        cat("\t\t\t Speed up factor \n")
+        cat("\t\t\t Speed up factor (S) of a line \n")
         print.default(format(SpeedTable,digits = 3),print.gap = 2L, 
 						quote = FALSE)
-		  
-        cat("\n Maximum theoretical attainable improvement in execution time:\n\n")
-        cat("\t\t\t Speed up factor \n")
+        cat("Lowest attainable execution time for the entire program when\n             lines are sped-up with factor (S):\n\n" 		  
+        cat("\t\t\t Speed up factor (S) of a line  \n")
         print.default(format(ExecTimeTable,digits = 3),print.gap = 2L, 
 						quote = FALSE)
         cat("\n    Total sampling time: ",round(LineProf$Total.Time,2) ," seconds")
